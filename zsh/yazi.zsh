195c34d80c86ac404d8cb6d32d6e81471bff0384
namu <namutaka+g@gmail.com>
2026-07-21 15:57:33 +0900

    yazi導入

---
+++ zsh/yazi.zsh
@@ -0,0 +1,14 @@
+#
+# yazi
+# https://yazi-rs.github.io/docs/quick-start
+#
+
+# change directory
+function yy() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  command yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd < "$tmp"
+  [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+  rm -f -- "$tmp"
+}
+
